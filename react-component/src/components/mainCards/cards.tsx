63b0ef88b5f169aca4f2a0a1cffd9874b6d36c55
DianaSmertina <am.dianasmertina@gmail.com>
2023-04-08 12:54:07 +0400

    feat: activate search

--- react-component/src/components/mainCards/cards.tsx
+++ react-component/src/components/mainCards/cards.tsx
@@ -3,7 +3,7 @@ import { Card } from './cardTemplate';
 import { Character, CharacterResponseResult } from '../../types/types';
 import { Loading } from './loading';
 
-export function CardList() {
+export function CardList(props: { search: string }) {
   const [cards, setCards] = useState<{ isLoaded: boolean; result: CharacterResponseResult | null }>(
     { isLoaded: false, result: null }
   );
@@ -15,6 +15,18 @@ export function CardList() {
         setCards({ isLoaded: true, result: data });
       });
   }, []);
+
+  useEffect(() => {
+    setCards((prevState) => {
+      return { isLoaded: false, result: prevState.result };
+    });
+    const savedText = localStorage.getItem('search') || '';
+    fetch('https://rickandmortyapi.com/api/character/?name=' + savedText.toLowerCase())
+      .then((response) => response.json())
+      .then((data) => {
+        setCards({ isLoaded: true, result: data });
+      });
+  }, [props.search]);
 
   const getCardsElem = (cardsArr: Array<Character> | undefined) => {
     if (cardsArr) {
--- react-component/src/pages/mainPage.tsx
+++ react-component/src/pages/mainPage.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SearchBar } from '../components/searchBar';
 import { CardList } from '../components/mainCards/cards';
 
 export function MainPage() {
+  const [searchText, setSearchText] = useState('');
+
+  const mainHandleSubmit = (text: string) => {
+    setSearchText(text);
+  };
+
   return (
     <main>
       <h1>Main page</h1>
-      <SearchBar />
-      <CardList />
+      <SearchBar onSumbit={(text: string) => mainHandleSubmit(text)} />
+      <CardList search={searchText} />
     </main>
   );
 }
