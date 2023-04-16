512b7f9f151cdffb28f8f1e674aeb67aff3dbe22
DianaSmertina <am.dianasmertina@gmail.com>
2023-04-16 21:39:05 +0400

    fix: fix bug with loading indicators and some style

--- react-component/src/components/mainCards/cards.tsx
+++ react-component/src/components/mainCards/cards.tsx
@@ -9,7 +9,7 @@ import { Loading } from './loading';
 export function CardList(props: { onClick: (cardId: number) => void }) {
   const searchText = useSelector((state: RootState) => state.search.searchText);
   const savedText = searchText || '';
-  const { data, isLoading, isError } = useGetCharactersByNameQuery(savedText);
+  const { data, isError, isFetching } = useGetCharactersByNameQuery(savedText);
 
   const getCardsElem = (cardsArr: Array<Character> | undefined) => {
     if (cardsArr) {
@@ -21,9 +21,9 @@ export function CardList(props: { onClick: (cardId: number) => void }) {
 
   return (
     <div className="cards">
-      {isLoading && <Loading />}
+      {isFetching && <Loading />}
       {isError && 'Characters not found'}
-      {!isLoading && data && data.results.length > 0 && getCardsElem(data.results)}
+      {!isFetching && !isError && data && data.results.length > 0 && getCardsElem(data.results)}
     </div>
   );
 }
