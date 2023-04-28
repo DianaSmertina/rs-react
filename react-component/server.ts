20934a1b940262443892d69d1af8825b66320637
DianaSmertina <am.dianasmertina@gmail.com>
2023-04-28 13:01:57 +0400

    feat: change on dev

--- react-component/server.ts
+++ react-component/server.ts
@@ -3,9 +3,9 @@ import path from 'node:path';
 import fs from 'fs';
 import { fileURLToPath } from 'node:url';
 import { createServer as createViteServer } from 'vite';
-import render from './dist/server/ServerApp.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 5173;
 
 async function createServer() {
   const app = express();
@@ -17,37 +17,35 @@ async function createServer() {
 
   app.use(vite.middlewares);
 
-  app.use('/assets', express.static(path.resolve(__dirname, './dist/client/assets')));
+  // app.use('/assets', express.static(path.resolve(__dirname, './dist/client/assets')));
 
-  const html = fs
-    .readFileSync(path.resolve(__dirname, './dist/client/index.html'), 'utf-8')
-    .toString();
+  const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8').toString();
 
   const parts = html.split('<!--ssr-outlet-->');
+
+  const { render } = await vite.ssrLoadModule('/src/server/ServerApp.tsx');
 
   app.use((req, res) => {
     res.write(parts[0]);
     const stream = render(req.url, {
       onShellReady() {
-        console.log(parts[0]);
         stream.pipe(res);
       },
       onShellError() {
         // do error handling
       },
       onAllReady() {
-        console.log(parts[1]);
         res.write(parts[1]);
         res.end();
       },
-      onError(err) {
+      onError(err: Error) {
         console.error(err);
       },
     });
   });
 
-  app.listen(5173, () => {
-    console.log('Server is running');
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 }
 
--- react-component/src/server/ServerApp.tsx
+++ react-component/src/server/ServerApp.tsx
@@ -1,11 +1,11 @@
 import { renderToPipeableStream } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
-import App from './App';
+import App from '../App';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { setupStore } from './redux/store';
+import { setupStore } from '../redux/store';
 
-export default function render(url: string, opts?: object) {
+export function render(url: string, opts?: object) {
   const stream = renderToPipeableStream(
     <Provider store={setupStore()}>
       <StaticRouter location={url}>
